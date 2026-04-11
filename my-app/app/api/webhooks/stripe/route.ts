19e72cd3141fdbe8a05309abfc9eab93c7e7e0f4
Apoorva pratap singh <apoorvapratapsingh6@gmail.com>
2026-04-12 03:50:30 +0530

    Updated apiVersion

--- my-app/app/api/webhooks/stripe/route.ts
+++ my-app/app/api/webhooks/stripe/route.ts
@@ -5,7 +5,7 @@ import { createClient } from "@supabase/supabase-js";
 export const runtime = "nodejs";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-04-30.basil",
+  apiVersion: "2026-02-25.clover",
 });
 
 const supabase = createClient(
